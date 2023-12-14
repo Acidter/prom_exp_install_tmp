c81eeca343b60caddf4c2ac7009cd59f8228396c
Dmitriy Khokhlov <acidterr@gmail.com>
2023-12-15 00:05:19 +0400

    change method get primary ip, bump version and small fix

--- process_exporter_install.sh
+++ process_exporter_install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -xe
 
-process_exporter_version=0.5.0
+process_exporter_version=0.7.10
 process_exporter_base_url=https://github.com/ncabatoff/process-exporter/releases/download/
 process_exporter_get_url=${process_exporter_base_url}v${process_exporter_version}/process-exporter-${process_exporter_version}.linux-amd64.tar.gz
-higs_ip=$(curl -s http://checkip.amazonaws.com)
+higs_ip=$(ip route get 1.1.1.1 | awk '$6 ~ /src/ {print $7}')
 process_exp_port=9256
 
 mkdir -p ~/tmp_deb
 wget ${process_exporter_get_url} -O tmp_deb/process_exporter-${process_exporter_version}.linux-amd64.tar.gz
 mkdir ~/tmp_deb/process_exporter_bin
 tar -xvf ~/tmp_deb/process_exporter-${process_exporter_version}.linux-amd64.tar.gz -C ~/tmp_deb/process_exporter_bin/
-mv tmp_deb/process_exporter_bin/process-exporter-${process_exporter_version}.linux-amd64/process-exporter /usr/local/bin/
+mv ~/tmp_deb/process_exporter_bin/process-exporter-${process_exporter_version}.linux-amd64/process-exporter /usr/local/bin/
 rm -rfv ~/tmp_deb
 
 cat <<EOF > /etc/systemd/system/process_exporter.service
